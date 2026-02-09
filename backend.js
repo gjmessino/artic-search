c376b07d8e0d7cb453e0a6e879b8064607e54bb1
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-02-09 14:57:17 -0800

    Refactor fetchData and add input validation functions
    Updated fetchData function to handle input and fetch logic. Added checkInput and checkFetch functions for input validation and retrying fetch on failure.

--- backend.js
+++ backend.js
@@ -1,34 +1,65 @@
 async function fetchData(){
     try{
         let art_id;
-        if(!art_id){
+        try{
+            art_id = document.getElementById("art_id").value;
+        }
+        catch(error){
+            console.log("No input provided")
+        }
+        if (!art_id){
             art_id = Math.floor(Math.random() * 10000)
         }
-        else {
-            parseInt(art_id = document.getElementById("art_id").value);
+        data = await checkFetch(art_id)
+        for (const keys in data.data){
+            if (data.data[keys] == null || data.data[keys] == undefined){
+                data.data[keys] = "Unknown"
+            }
         }
-        const response = await fetch(`https://api.artic.edu/api/v1/artworks/${art_id}`)
-        if(!response.ok){
-            throw new Error ("Could not fetch resource")
-        }
-        const data = await response.json()
-        const image_id = data.image_id
+        // Find image url and display image
+        const image_id = String(data.data.image_id)
         const image_url = `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`
         const image_element = document.getElementById("art_image")
         image_element.src = image_url
         image_element.style.display = "block"
-        const title = data.title
+        // Find title, artist, and description and display
+        const title = String(data.data.title)
         const title_element = document.getElementById("art_title")
         title_element.innerHTML = title
-        const artist = data.artist_title
+        const artist = String(data.data.artist_title)
         const artist_element = document.getElementById("artist_name")
         artist_element.innerHTML = artist
-        const description = data.thumbnail.alt_text
+        const description = String(data.data.description)
         const description_element = document.getElementById("art_description")
         description_element.innerHTML = description
     }
     catch(error){
         console.error(error)
     }
+}
+
+function checkInput(){
+    art_id = document.getElementById("art_id").value;
+    if (isNaN(art_id)){
+        alert("Please enter a valid number")
+    }
+    else if (art_id <1000 || art_id > 123199){
+        alert("Please enter a valid birthday")
+    }
+    else {
+        fetchData()
+    }
+}
+
+async function checkFetch(art_id){
+    let response = await fetch(`https://api.artic.edu/api/v1/artworks/${art_id}`)
+        if(!response.ok){
+            while (!response.ok){
+                art_id = Math.floor(Math.random() * 10000)
+                response = await fetch(`https://api.artic.edu/api/v1/artworks/${art_id}`)
+            }
+        }
+        let data = await response.json()
+    return data
 }
 
